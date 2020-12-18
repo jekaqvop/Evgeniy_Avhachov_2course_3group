e4a98800e8e1fd2442dbf0dc4baf0f9f7577e454
jekaqvop <jekaavhachev@gmail.com>
2020-12-18 21:16:39 +0300

    -

---
+++ лаба 15/лаба 15/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace лаба_15
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n------------- Task 1 --------------");
+
+            Process[] AllProcesses = Process.GetProcesses();
+            ThreadClass.Process(AllProcesses);
+            Console.WriteLine(ThreadClass.InfoDomain());
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.WriteLine("\n------------- Task 2 --------------");
+            Console.WriteLine("Create new Domain");
+            AppDomain newDomain = AppDomain.CreateDomain("NewMyDomain");
+            //Создание нового домена
+            Console.WriteLine("assembly To Load");
+            Assembly assemblyToLoad = Assembly.LoadFrom("ClassLibrary1.dll");
+            
+            //Загрузка сборки в новый домен
+            newDomain.Load(assemblyToLoad.FullName);
+            Console.WriteLine("assembly UnLoad");
+            //Выгрузка домена
+            AppDomain.Unload(newDomain);            
+            try
+            {
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+                //NumbersThread.Suspend();               
+                //NumbersThread.Resume();                
+                Console.WriteLine("--------- Task 3 ----------");
+                Thread thread = new Thread(new ParameterizedThreadStart(ThreadClass.PrimeNumbers));
+                thread.Name = "OutPrimeNumbers";
+                thread.Start(1000);
+                Console.WriteLine("Its priority: " + thread.Priority);
+                Thread.Sleep(100);
+                Console.WriteLine("Its name: " + thread.Name);
+                Thread.Sleep(100);
+                Console.WriteLine("Its ID: " + thread.ManagedThreadId);
+                Thread.Sleep(1000);
+                thread.Abort();
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+                Console.WriteLine("\n------------- Task 4 --------------");
+
+                Console.WriteLine("Consistently:");
+                OddEven.DoItConsistently();
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+                
+                
+                Console.WriteLine("\nOne by one:");
+                OddEven.DoItOneByOne();
+                Thread.Sleep(5000);            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.WriteLine("\n------------- Task 5 --------------");
+            Console.WriteLine("Timer");
+            //Timer
+            Console.WriteLine("\nPress any key");
+            Console.ReadKey();
+
+            TimerCallback timerCallback = new TimerCallback(WhatTimeIsIt);
+
+            Timer timer = new Timer(timerCallback, null, 0, 1000);   
+            
+            void WhatTimeIsIt(object obj)
+            {
+                Console.WriteLine($"It's {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            }
+
+            Console.WriteLine("\nPress any key for exit");            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ лаба 15/лаба 15/TheardClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace лаба_15
+{
+    class ThreadClass
+    {
+        public static void PrimeNumbers(object N)
+        {                         
+            using (StreamWriter file = new StreamWriter("PrimeNumbers.txt"))
+            {                
+                //Нахождение простых чисел
+                for (int i = 2; i <= (int)N; i++)
+                {
+                    if (IsSimple(i))
+                    {
+                        Thread.Sleep(100);
+                        Console.Write(i.ToString() + ", ");
+                        file.WriteLine(i.ToString());
+                    }
+                }
+
+                file.Close();
+            }
+            Console.WriteLine("PrimeNumbers stopped");
+        }
+    
+            static bool IsSimple(int N)
+            {
+            bool tf = false;            
+            for (int i = 2; i < (int)(N / 2); i++)
+            {
+                if (N % i == 0)
+                {
+                    tf = false;
+                    break;
+                }
+                else
+                {
+                    tf = true;
+                }
+            }
+            return tf;
+        }
+        public static void WriteFile(string str, string path = "writer")
+        {
+            using (StreamWriter writeFile = new StreamWriter(path))
+            {
+                writeFile.WriteLine(str);
+            }
+        }
+        public static string Process(params Process[] process)
+        {
+            string str = "";
+            
+            foreach (Process pr in process)
+            {
+                try
+                {
+                        str += "\nID:" + pr.Id.ToString();
+                        str += "\nProcessName: " + pr.ProcessName;
+                        str += "\nStartInfo" + pr.StartInfo;
+                        str += "\nStartTime" + pr.StartTime;
+                        str += "\nBasePriority" + pr.BasePriority;
+                        str += "\nUserProcessorTime" + pr.UserProcessorTime;
+                        str += "\nVirtualMemorySize" + (pr.VirtualMemorySize64 / 1048576).ToString();
+                        str += "\nPagedMemorySize64" + (pr.PagedMemorySize64 / 1048576).ToString() + "\n";
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Ошибка: {0}", e.Message);
+                }
+                
+                
+            }            
+            return str;
+        }
+        public static string InfoDomain()
+        {
+            string str = "\nFriendlyName: " 
+                + AppDomain.CurrentDomain.FriendlyName 
+                + "\nSetupInformation: " 
+                + AppDomain.CurrentDomain.SetupInformation;
+            str += "\nAssemblies: ";
+            foreach (var x in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                str += x + "\n";
+            }
+            return str;
+        }        
+
+    }  
+}
